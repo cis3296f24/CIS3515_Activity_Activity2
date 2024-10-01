ac0f6b2935697056748a30ef2a55e769de54bf82
Youngdoul <157045613+Youngdoul@users.noreply.github.com>
2024-10-01 11:49:42 -0400

    Update DisplayActivity.kt

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -11,6 +11,8 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
+              if (message != null) {
+                textSize = message.toFloat()
 
         }
 
